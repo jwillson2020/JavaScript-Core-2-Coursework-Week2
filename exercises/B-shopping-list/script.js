9ffceee684c58f358ac0f224b89b25b785671e86
jwillson2020 <73646450+jwillson2020@users.noreply.github.com>
2021-01-20 11:29:57 +0000

    add css colour to list

--- exercises/B-shopping-list/script.js
+++ exercises/B-shopping-list/script.js
@@ -1,5 +1,19 @@
+
+
 function shoppingList(arrayOfPeople) {
-  // Write your code here...
+let content = document.querySelector("#content");
+document.body.appendChild(content);
+let unOrdered = document.createElement("UL");
+document.getElementById("content").appendChild(unOrdered);
+// Write your code here...
+
+  for(let i = 0; i < shopping.length; i++){
+    let  list = document.createElement("LI");  
+    //array[i] = shopping[i];
+    shoppingList.innerHTML = `${shopping[i].name}`;
+   unOrdered.appendChild(list);
+ }
+
 }
 
 let shopping = ["Milk", "Bread", "Eggs", "A Dinosaur", "Cake", "Sugar", "Tea"];
